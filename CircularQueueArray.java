0c0ffa0fd83d6aa0774556d5d21334d803be87b1
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2026-07-01 00:00:44 +0530

    check remove function

--- CircularQueueArray.java
+++ CircularQueueArray.java
@@ -61,5 +61,11 @@ public class CircularQueueArray{
       q1.Add(15);
       q1.Add(15);
   System.out.println(q1.size);
+  q1.Remove();
+  q1.Remove();
+  q1.Remove();
+  q1.Remove();
+  q1.Remove();
+  q1.Remove();
     }
 }
